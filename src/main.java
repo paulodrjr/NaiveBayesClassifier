d3b01ede74dfad019571fd55f77ea87d805ee1b7
paulodrjr <paulodrjr@gmail.com>
2010-11-10 20:38:30 -0200

    alterações interessantes

--- src/main.java
+++ src/main.java
@@ -15,6 +15,7 @@ import java.util.Iterator;
 import java.util.Hashtable;
 import java.util.Set;
 import java.util.StringTokenizer;
+import java.util.concurrent.Semaphore;
 
 public class main {
 
@@ -31,17 +32,59 @@ public class main {
 	static int matchCount = 0;
 	static int classificationSamples = 0;
 
+	private static boolean hasCharRepetitions(String word, int maxRepetitions) {
+		int repetitions = 1;
+		char buffer = '#';
+		for (char c : word.toCharArray()) {
+			if (c == buffer)
+				repetitions++;
+			else {
+				buffer = c;
+				repetitions = 1;
+			}
+			if (repetitions == maxRepetitions) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 	/**
 	 * Retorna palavras "válidas", ou string vazio caso contrário.
 	 * 
-	 * @param str
+	 * @param word
 	 * @return
 	 */
-	public static String getValidWord(String str) {
+	public static String getValidWord(String word) {
 		// deixar de fora palavras com tamanho menor ou igual a 3 ou maior que
 		// 44
-		
-		if ((str.length() < 3)  || (str.length() > 44) )
+
+		if ((word.length() < 3) || (word.length() > 44))
+			return "";
+		if (hasCharRepetitions(word, 3))
+			return "";
+		StringBuilder sb = new StringBuilder();
+		// deixar de fora palavras com caracteres inválidos
+		for (char c : word.toCharArray()) {
+			if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
+				sb.append(c);
+			} else {
+				// if (c == '@')
+				return "";
+			}
+		}
+		if ((sb.length() < 3) || (sb.length() > 44))
+			return "";
+		if (hasCharRepetitions(sb.toString(), 3))
+			return "";
+		return sb.toString();
+	}
+
+	public static String getValidWordVocabulary(String str) {
+		// deixar de fora palavras com tamanho menor ou igual a 3 ou maior que
+		// 44
+
+		if ((str.length() < 3) || (str.length() > 44))
 			return "";
 		StringBuilder sb = new StringBuilder();
 		// deixar de fora palavras com caracteres inválidos
@@ -49,26 +92,7 @@ public class main {
 			if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
 				sb.append(c);
 			} else {
-				if (c == '@')
-					return "";
-			}
-		}
-		return sb.toString();
-	}
-	
-	public static String getValidWordVocabulary(String str) {
-		// deixar de fora palavras com tamanho menor ou igual a 3 ou maior que
-		// 44
-		
-		if ((str.length() < 3)  || (str.length() > 44) )
-			return "";
-		StringBuilder sb = new StringBuilder();
-		// deixar de fora palavras com caracteres inválidos
-		for (char c : str.toCharArray()) {
-			if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
-				sb.append(c);
-			} else {				
-					return "";
+				return "";
 			}
 		}
 		return sb.toString();
@@ -90,8 +114,9 @@ public class main {
 		while (st.hasMoreElements()) {
 
 			// tenta eliminar tokens indesejáveis
-			word = getValidWordVocabulary(st.nextToken().toLowerCase().trim());
-//			word = st.nextToken().toLowerCase().trim();
+//			 word =
+//			 getValidWordVocabulary(st.nextToken().toLowerCase().trim());
+			word = st.nextToken().toLowerCase().trim();
 
 			int i = word.compareTo(EmptyStr);
 			if (i == 0)
@@ -107,7 +132,7 @@ public class main {
 		System.out.println("Creating vocabulary done.");
 
 		return cleanVocabulary(map);
-//		return map;
+		// return map;
 	}
 
 	/**
@@ -124,7 +149,7 @@ public class main {
 		Iterator<String> it = map.keySet().iterator();
 		ArrayList<String> list = new ArrayList<String>();
 		while (it.hasNext()) {
-			String key = it.next();			
+			String key = it.next();
 			if (map.get(key) < 3)
 				list.add(key);
 		}
@@ -177,22 +202,23 @@ public class main {
 
 	}
 
-	public static boolean verificaCabecalho(String valor) {
+	public static boolean isHeader(String valor) {
 		valor = valor.toLowerCase().trim();
-		return ((!(valor.contains("subject:")))
-				&& (!(valor.contains("expires:")))
-				&& (!(valor.contains("distribution:")))
-				&& (!(valor.contains("lines:")))
-				&& (!(valor.contains("from:")))
-				&& (!(valor.contains("article-i.d.:")))
-				&& (!(valor.contains("reply-to:")))
-				&& (!(valor.contains("organization:")))
-				&& (!(valor.contains("nntp-posting-host:")))
-				&& (!(valor.contains("organization:")))
-//				&& (!(valor.contains("an"))) && (!(valor.contains("and")))
-//				&& (!(valor.contains("as"))) && (!(valor.contains("at")))
-//				&& (!(valor.contains("be"))) && (!(valor.contains("but")))
-//				&& (!(valor.contains("by"))) && (!(valor.contains("i")))
+		return (((valor.contains("subject:")))
+				&& ((valor.contains("expires:")))
+				&& ((valor.contains("distribution:")))
+				&& ((valor.contains("lines:")))
+				&& ((valor.contains("from:")))
+				&& ((valor.contains("article-i.d.:")))
+				&& ((valor.contains("in article")))
+				&& ((valor.contains("reply-to:")))
+				&& ((valor.contains("organization:")))
+				&& ((valor.contains("nntp-posting-host:"))) && ((valor
+				.contains("organization:")))
+		// && (!(valor.contains("an"))) && (!(valor.contains("and")))
+		// && (!(valor.contains("as"))) && (!(valor.contains("at")))
+		// && (!(valor.contains("be"))) && (!(valor.contains("but")))
+		// && (!(valor.contains("by"))) && (!(valor.contains("i")))
 
 		);
 	}
@@ -208,35 +234,42 @@ public class main {
 		BufferedReader in;
 		try {
 			in = new BufferedReader(new FileReader(fileName));
-			String str;
-			StringBuffer r = new StringBuffer();
-			boolean startCommentsRead = false;
+			try {
+				String str;
+				StringBuffer r = new StringBuffer();
+				boolean startCommentsRead = false;
 
-			// tenta deixar de fora o cabeçalho do e-mail, pulando todas as
-			// linhas até encontrar uma linha em branco.
-			// while (((str = in.readLine()) != null) && !startCommentsRead) {
-			// startCommentsRead = (str.trim().compareTo("") == 0);
-			// }
-			while ((str = in.readLine()) != null) {
-				if (verificaCabecalho(str)) {
-					StringTokenizer st = new StringTokenizer(str);
-					String cleanStr = "";
-					while (st.hasMoreElements()) {
-						String token = st.nextToken();
-						cleanStr += getValidWord(token).trim().toLowerCase()
-								+ " ";
+				// tenta deixar de fora o cabeçalho do e-mail, pulando todas as
+				// linhas até encontrar uma linha em branco.
+				// while (((str = in.readLine()) != null) && !startCommentsRead)
+				// {
+				// startCommentsRead = (str.trim().compareTo("") == 0);
+				// }
+				while ((str = in.readLine()) != null) {
+					if (!isHeader(str)) {
+						StringTokenizer st = new StringTokenizer(str);
+						String cleanStr = "";
+						while (st.hasMoreElements()) {
+							String token = st.nextToken();
+							cleanStr += getValidWord(token).trim()
+									.toLowerCase()
+									+ " ";
+						}
+						r.append(cleanStr);
 					}
-					r.append(cleanStr);
 				}
-			}
-			File file = new File(fileName);
+				File file = new File(fileName);
 
-			File dir = new File(file.getParent() + "/output/");
-			if (!dir.exists())
-				dir.mkdir();
-			writeTextFile(r.toString(),
-					dir.getAbsolutePath() + "/" + file.getName());
-			return r.toString();
+				File dir = new File(file.getParent() + "/output/");
+				if (!dir.exists())
+					dir.mkdir();
+				writeTextFile(r.toString(), dir.getAbsolutePath() + "/"
+						+ file.getName());
+				return r.toString();
+			} finally {
+				in.close();
+			}
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -245,48 +278,6 @@ public class main {
 
 	}
 	
-	public static String cleanTextFile_Test(String fileName) {
-
-		BufferedReader in;
-		try {
-			in = new BufferedReader(new FileReader(fileName));
-			String str;
-			StringBuffer r = new StringBuffer();
-			boolean startCommentsRead = false;
-
-			// tenta deixar de fora o cabeçalho do e-mail, pulando todas as
-			// linhas até encontrar uma linha em branco.
-			// while (((str = in.readLine()) != null) && !startCommentsRead) {
-			// startCommentsRead = (str.trim().compareTo("") == 0);
-			// }
-			while ((str = in.readLine()) != null) {
-				
-					StringTokenizer st = new StringTokenizer(str);
-					String cleanStr = "";
-					while (st.hasMoreElements()) {
-						String token = st.nextToken();
-						cleanStr += token.trim().toLowerCase()
-								+ " ";
-					}
-					r.append(cleanStr);
-				
-			}
-			File file = new File(fileName);
-
-			File dir = new File(file.getParent() + "/output/");
-			if (!dir.exists())
-				dir.mkdir();
-			writeTextFile(r.toString(),
-					dir.getAbsolutePath() + "/" + file.getName());
-			return r.toString();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return null;
-		}
-
-	}
-
 	/**
 	 * Escreve um arquivo de texto
 	 * 
@@ -321,18 +312,7 @@ public class main {
 		}
 
 	}
-	
-	private static void CleanFiles_Test(String dir) {
-		File folder = new File(dir);
-		File[] listOfFiles = folder.listFiles();
-		System.out.println("Cleaning text files from directory " + dir);
-		for (int i = 0; i < listOfFiles.length; i++) {
-			if (listOfFiles[i].isFile()) {
-				cleanTextFile_Test(listOfFiles[i].getAbsolutePath());
-			}
-		}
 
-	}
 	/**
 	 * lê o arquivo de vocabulário para o vocabulário em memória
 	 * 
@@ -444,8 +424,8 @@ public class main {
 	 * @param trainDirs
 	 * @param testDirs
 	 * @param recreateVocabulary
-	 * @return um array de string, onde cada i-ésima posição contém o
-	 *         conteúdo concatenado dos textos da classe
+	 * @return um array de string, onde cada i-ésima posição contém o conteúdo
+	 *         concatenado dos textos da classe
 	 */
 	private static String[] prepare(String[] trainDirs, String[] testDirs,
 			boolean recreateVocabulary) {
@@ -467,8 +447,12 @@ public class main {
 			// efetua uma limpeza nos arquivos, direcionando a saída para o
 			// subdiretório "output" de cada classe
 			for (int i = 0; i < trainDirs.length; i++) {
-				CleanFiles_Test(trainDirs[i]);
-				CleanFiles_Test(testDirs[i]);
+				CleanFiles(trainDirs[i]);
+			}
+
+			for (int i = 0; i < trainDirs.length; i++) {
+
+				CleanFiles(testDirs[i]);
 			}
 
 			// using Threads in order to improve reading performance
@@ -493,11 +477,11 @@ public class main {
 
 			// olhar o algoritmo presente no texto-base de bayes
 			Pv = new double[readers.length];
-			
+
 			for (int i = 0; i < readers.length; i++)
 				nExamples += readers[i].count;
-			
-			for (int i = 0; i < readers.length; i++) {				
+
+			for (int i = 0; i < readers.length; i++) {
 				results[i] = readers[i].getFilesContent() + "\n";
 				writeTextFile(results[i], localdir.getCanonicalPath()
 						+ "/files/" + internalClassNames[i] + ".text");
